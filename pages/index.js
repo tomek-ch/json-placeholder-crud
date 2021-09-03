8c71f31fb509d0481c5df900b90ce0590b62628c
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-03 10:57:13 +0200

    Display a list of posts on the front page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <h1>Hello world</h1>;
+import { postsList } from "../styles/PostsList.module.scss";
+
+export default function Home({ posts, error }) {
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
+  return (
+    <ul className={postsList}>
+      {posts.map(({ id, title }) => (
+        <li key={id}>{title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getServerSideProps() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (res.status === 200) {
+      return { props: { posts: await res.json() } };
+    }
+  } catch {}
+
+  return { props: { error: "Server error" } };
 }
