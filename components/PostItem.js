09c447bed53cf944b8136db624d2136eb75549fb
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-03 12:45:17 +0200

    Style post item buttons

--- components/PostItem.js
+++ components/PostItem.js
@@ -1,6 +1,8 @@
 import { postItem } from "../styles/components/PostItem.module.scss";
 import Link from "next/link";
 import { useState } from "react";
+import { btn } from "../styles/components/BtnPrimary.module.scss";
+import { btnDanger } from "../styles/components/BtnDanger.module.scss";
 
 export default function PostItem({ title, id, setPosts }) {
   const [error, setError] = useState("");
@@ -27,8 +29,12 @@ export default function PostItem({ title, id, setPosts }) {
       <div className={postItem}>
         <Link href={`/posts/${id}`}>{title}</Link>
         <div>
-          <button onClick={remove}>Delete</button>
-          <Link href={`/posts/${id}/edit`}>Update</Link>
+          <button className={btnDanger} onClick={remove}>
+            Delete
+          </button>
+          <Link href={`/posts/${id}/edit`}>
+            <a className={btn}>Update</a>
+          </Link>
         </div>
       </div>
       <div>{error}</div>
