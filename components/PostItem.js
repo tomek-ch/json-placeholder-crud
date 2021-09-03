458d4a0c909afa01d673c55b0e91281671aef465
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-09-03 12:07:10 +0200

    Add buttons to post

--- components/PostItem.js
+++ components/PostItem.js
@@ -5,6 +5,10 @@ export default function PostItem({ title, id }) {
   return (
     <div className={postItem}>
       <Link href={`/posts/${id}`}>{title}</Link>
+      <div>
+        <button>Delete</button>
+        <Link href={`/posts/${id}/update`}>Update</Link>
+      </div>
     </div>
   );
 }
